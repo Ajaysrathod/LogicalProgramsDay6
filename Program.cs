796a5e9e21bf5b5e9477dd8e91428b73bc30c89f
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-04-28 22:49:58 +0530

    Welcome to Fibonacci series

--- Program.cs
+++ Program.cs
@@ -5,6 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical Programs!");
+            Console.WriteLine("please enter below option ");
+            Console.WriteLine("1:Fibonacci");
+            int options = Convert.ToInt32(Console.ReadLine());
+            switch (options)
+            {
+                case 1:
+                    Console.WriteLine("Please enter input value");
+                    int count = Convert.ToInt32(Console.ReadLine());
+                    Fibonacci.FibonacciNum(count);
+                    break;
+                default:
+                    Console.WriteLine("Plaese choose correct option");
+                    break;
+            }
         }
     }
 }
