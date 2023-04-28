7b61a79f12e4ff9f699b5754b6609e926d800b1b
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-04-28 22:40:35 +0530

    Welcome to Logical Programs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LogicalProgramsDay6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programs!");
+        }
+    }
+}
