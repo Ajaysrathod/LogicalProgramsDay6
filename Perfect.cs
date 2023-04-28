eeee103b673d9f2b3445898064186abe96ede22a
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-04-28 22:58:33 +0530

    Welcome to Perfect Number

---
+++ Perfect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramsDay6
+{
+    public class Perfect  //Creating class
+    {
+        public static void PerfectNumber1(int number)       //Creating method
+        {
+            int sum = 0;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+
+            if (sum == number)
+                Console.WriteLine("{0} is perfect number", number);
+            else
+                Console.WriteLine("{0} is not perfect number", number);
+        }
+    
+    }
+}
